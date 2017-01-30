03a7b791f4fe64e83e751a1f0cfb111a88cb5a23
Anixen <anixen-dev@lapsote.net>
2017-01-30 02:08:02 +0100

    Implemented a first basic movement system relying on SWS.
    User can interact with triggers to launch associated spline movement.
    Splines are calculated using Simple Waypoint System.
    (I gonna buy the licence, this plugin is definitely worth its 15 bucks)
    
    The final movement system should be more advanced :
    - encapsulate the splines into a dedicated class for more flexibility ?
    - define the posible standing position (as nodes) in the level and what splines (edges) connect them
    (this nodes concept is central in the level design as it could handle events when the player enters/leaves the position)
    - create a helper class/script to manage/build the whole graph from external data/input and shorten development time ?

---
+++ Assets/Scripts/NavGraphEdgeTrigger.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using SWS;
+
+[RequireComponent(typeof(Collider))]
+public class NavGraphEdgeTrigger : MonoBehaviour, IGvrGazeResponder
+{
+    // Reference to path
+    public PathManager pathContainer;
+
+    public Material inactiveMaterial;
+    public Material gazedAtMaterial;
+
+    //private float rotationSpeed;
+
+    void Start()
+    {
+        SetGazedAt(false);
+    }
+
+    void LateUpdate()
+    {
+        GvrViewer.Instance.UpdateState();
+        if (GvrViewer.Instance.BackButtonPressed)
+        {
+            Application.Quit();
+        }
+    }
+
+    public void SetGazedAt(bool gazedAt)
+    {
+        if (inactiveMaterial != null && gazedAtMaterial != null)
+        {
+            GetComponent<Renderer>().material = gazedAt ? gazedAtMaterial : inactiveMaterial;
+            return;
+        }
+        GetComponent<Renderer>().material.color = gazedAt ? Color.green : Color.red;
+    }
+
+    // TODO Trigger movement function here
+    public void StartMovement(splineMove movement)
+    {
+        movement.pathContainer = this.pathContainer;
+        movement.reverse = false;
+        movement.StartMove();
+    }
+
+    public void StartMovement_Reverse(splineMove movement)
+    {
+        movement.pathContainer = this.pathContainer;
+        movement.reverse = true;
+        movement.StartMove();
+    }
+
+    #region IGvrGazeResponder implementation
+
+    /// Called when the user is looking on a GameObject with this script,
+    /// as long as it is set to an appropriate layer (see GvrGaze).
+    public void OnGazeEnter()
+    {
+        SetGazedAt(true);
+    }
+
+    /// Called when the user stops looking on the GameObject, after OnGazeEnter
+    /// was already called.
+    public void OnGazeExit()
+    {
+        SetGazedAt(false);
+    }
+
+    /// Called when the viewer's trigger is used, between OnGazeEnter and OnPointerExit.
+    public void OnGazeTrigger()
+    {
+        //TODO Trigger movement
+    }
+
+    #endregion
+}
---
+++ Assets/Scripts/WalkerMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+using SWS;
+
+public class WalkerMovement : MonoBehaviour
+{
+    public splineMove walker;
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	    transform.position += walker.transform.localPosition;
+	    walker.transform.localPosition = Vector3.zero;
+	}
+}
