54103eb7d9793c35786872b45c60d38c2d1100b9
Anixen <anixen-dev@lapsote.net>
2017-02-07 19:18:28 +0100

    Added a custom Editor for NavGrapManager.
    The main addition is a button to update every Edge spline in a split-second.
    (See previous commit about updateSpline())

---
+++ Assets/Scripts/Editor/NavGraphManagerEditor.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+[CustomEditor(typeof(NavGraphManager))]
+public class NavGraphManagerEditor : Editor {
+
+    public override void OnInspectorGUI() {
+        GUILayout.BeginVertical();
+
+        DrawDefaultInspector();
+
+        if (GUILayout.Button("Update Splines")) {
+            Debug.Log("Updating splines");
+                
+            NavGraphEdgeMovement[] edges = Target.gameObject.GetComponentsInChildren<NavGraphEdgeMovement>();
+            foreach (NavGraphEdgeMovement edge in edges)
+                edge.updateSpline();
+        }
+        GUILayout.EndVertical();
+    }
+
+    NavGraphManager Target { get { return target as NavGraphManager; } }
+}
