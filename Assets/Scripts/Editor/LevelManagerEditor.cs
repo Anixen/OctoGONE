4f9a54eec4d1b20b58004fcca84b5f9b28fdc021
Anixen <anixen-dev@lapsote.net>
2017-02-28 21:42:45 +0100

    Began implementing a App and a Level Manager object.
    The level manager need a screen fader, and loading screen.
    The level manager editor is mostly here for testing purposes (artificaially trigger level transition)

---
+++ Assets/Scripts/Editor/LevelManagerEditor.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(LevelManager))]
+public class LevelManagerEditor : Editor
+{
+
+    public override void OnInspectorGUI()
+    {
+        GUILayout.BeginVertical();
+
+
+        DrawDefaultInspector();
+
+        
+        if (GUILayout.Button("Load Level 00"))
+        {
+            Debug.Log("Loading Level 00");
+            Target.Load("level_00");
+        }
+
+        if (GUILayout.Button("Load Level 01"))
+        {
+            Debug.Log("Loading Level 01");
+            Target.Load("level_01");
+        }
+
+        if (GUILayout.Button("Load Level 02"))
+        {
+            Debug.Log("Loading Level 02");
+            Target.Load("level_02");
+        }
+        //*/
+        GUILayout.EndVertical();
+    }
+
+    LevelManager Target { get { return target as LevelManager; } }
+}
