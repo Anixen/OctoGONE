34982337937dbfb4821536ca1eca78b7329d07cd
scogny <so.cogny@gmail.com>
2017-03-01 15:38:40 +0100

    Ajout d'un prefab Rune.
    Ce prefabe dispose d'un gizmo, et contient un plan/mesh grille sur lequel on ira afficher les textures des runes.
    A faire ultérieurement : mettre une poignee pour déplacer les runes plus facilement. (Optionel, script editeur)

---
+++ Assets/Scripts/Rune.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rune : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnDrawGizmos() {
+		Gizmos.color = Color.cyan;
+		Gizmos.DrawWireCube(transform.position, new Vector3(0.8f, 0.8f, 0.1f));
+	}
+}
