d634ba98b02d7872eb3ec70c5e614083a6a78520
Anixen <anixen-dev@lapsote.net>
2017-03-03 00:20:15 +0100

    Created Triggerable and Trigger scripts.
    A triggerable is basically an Event that will call functions to change the scen state.
    A trigger is there to handle user input (pointer) and set off one (or more) triggerables.
    
    A triggerable holds several informations regarding whether he is ready to execute.
    The triggers should be enabled/disabled according to the position of the player in the scene.

---
+++ Assets/Scripts/Trigger.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Collider))]
+public class Trigger : MonoBehaviour
+{
+    private Collider collider;
+    public List<Triggerable> Triggerables;
+
+    void Awake()
+    {
+        collider = GetComponent<Collider>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void Enable()
+    {
+        collider.enabled = true;
+    }
+
+    public void Disable()
+    {
+        collider.enabled = false;
+    }
+
+    /// Called when the user is looking on a GameObject with this script,
+    /// as long as it is set to an appropriate layer (see GvrGaze).
+    public void OnGazeEnter()
+    {
+        // TODO Higlight the object : change its color, show its outline
+    }
+
+    /// Called when the user stops looking on the GameObject, after OnGazeEnter
+    /// was already called.
+    public void OnGazeExit()
+    {
+        // TODO Reset the highlight apllied to the object
+    }
+
+    /// Called when the viewer's trigger is used, between OnGazeEnter and OnPointerExit.
+    public void OnGazeTrigger()
+    {
+        // Execute the associated Triggerables
+        foreach (var t in Triggerables)
+        {
+            t.Execute();
+        }
+    }
+}
