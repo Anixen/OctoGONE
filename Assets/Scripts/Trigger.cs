841395cf76d6ba7d34e18dcf3cbac2a6a23323ba
Anixen <anixen-dev@lapsote.net>
2017-03-03 01:33:57 +0100

    Event entries and callbacks for Triggers are now set up automatically.
    Since all the triggers will use the same events and callbacks, it is best ton instantate them automatially at runtime.
    However debgging could be trickier, since the callbacks functions added at runtime are not displayed in the EventTrigger inspector.
    (The callbacks still work as intended, but this is quite deceiving on first sight)
    
    This will allow to gain some time in defintive since we only have to care about the Triggerers when setting up a Trigger.

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,8 +1,10 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.EventSystems;
 
 [RequireComponent(typeof(Collider))]
+[RequireComponent(typeof(EventTrigger))]
 public class Trigger : MonoBehaviour
 {
     public bool locked = false;
@@ -11,14 +13,32 @@ public class Trigger : MonoBehaviour
 
     public List<Triggerable> Triggerables;
 
-
     private Collider collider;
+    private EventTrigger eventTrigger;
 
 
     void Awake()
     {
         collider = GetComponent<Collider>();
+        eventTrigger = GetComponent<EventTrigger>();
+
+        // Set up entries here intead of doing it manually
+        EventTrigger.Entry pointerEnter_entry = new EventTrigger.Entry();
+        pointerEnter_entry.eventID = EventTriggerType.PointerEnter;
+        pointerEnter_entry.callback.AddListener(OnGazeEnter);
+        eventTrigger.triggers.Add(pointerEnter_entry);
+
+        EventTrigger.Entry pointerExit_entry = new EventTrigger.Entry();
+        pointerExit_entry.eventID = EventTriggerType.PointerExit;
+        pointerExit_entry.callback.AddListener(OnGazeExit);
+        eventTrigger.triggers.Add(pointerExit_entry);
+
+        EventTrigger.Entry pointerClick_entry = new EventTrigger.Entry();
+        pointerClick_entry.eventID = EventTriggerType.PointerClick;
+        pointerClick_entry.callback.AddListener(OnGazeTrigger);
+        eventTrigger.triggers.Add(pointerClick_entry);
     }
+
 
     public void Enable()
     {
@@ -42,20 +62,20 @@ public class Trigger : MonoBehaviour
 
     /// Called when the user is looking on a GameObject with this script,
     /// as long as it is set to an appropriate layer (see GvrGaze).
-    public void OnGazeEnter()
+    public void OnGazeEnter(BaseEventData data)
     {
         // TODO Higlight the object : change its color, show its outline
     }
 
     /// Called when the user stops looking on the GameObject, after OnGazeEnter
     /// was already called.
-    public void OnGazeExit()
+    public void OnGazeExit(BaseEventData data)
     {
         // TODO Reset the highlight apllied to the object
     }
 
     /// Called when the viewer's trigger is used, between OnGazeEnter and OnPointerExit.
-    public void OnGazeTrigger()
+    public void OnGazeTrigger(BaseEventData data)
     {
         // Check the triggerable is not locked and can be repeated
         if (locked
