f4b2f6107c9e35018ad28c2d595006e882ba3d8a
Anixen <anixen-dev@lapsote.net>
2017-03-09 00:36:04 +0100

    Heavily modified the Portes for open and close behavior.
    A Portal now has two trigerables behaviors :
    - activating the door frame will open/close it, i.e. reval/hide another iteractive object inside it.
    - this interactive surface sends the player to another level when he clicks on it.

---
+++ Assets/Scripts/PortalOpen.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PortalOpen : MonoBehaviour {
+
+    public bool isOpened { get { return (meshRenderer.enabled && trigger.enabled) ; } }
+
+    private GameObject portalSurface;
+    private MeshRenderer meshRenderer;
+    private Trigger trigger;
+
+    void Awake()
+    {
+        portalSurface = GetComponentInChildren<PortalEnter>().gameObject;
+        meshRenderer = portalSurface.GetComponent<MeshRenderer>();
+        trigger = portalSurface.GetComponent<Trigger>();
+    }
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void TogglePortal()
+    {
+        if(isOpened)
+            ClosePortal();
+        else
+            OpenPortal();
+    }
+
+    public void OpenPortal()
+    {
+        //Debug.Log("Open Portal :" + name);
+        portalSurface.GetComponent<MeshRenderer>().enabled = true;
+        portalSurface.GetComponent<Trigger>().Unlock();
+    }
+
+    public void ClosePortal()
+    {
+        //Debug.Log("Close Portal :" + name);
+        portalSurface.GetComponent<Trigger>().Lock();
+        portalSurface.GetComponent<MeshRenderer>().enabled = false;
+    }
+}
