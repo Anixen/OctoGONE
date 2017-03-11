71ad4191eb0dd02e596d70e3b32266d6da0ccc34
Anixen <anixen-dev@lapsote.net>
2017-03-11 17:12:52 +0100

    Level_03 : Scripted elevator behavior.
    Implemented plateform activation and movement relying upon an Animator.
    
    The player is moved along by adding it as a child element, and replacing is when it was in the scene hierachy when the movement is finished.
    (This might not be the cleanest method, but it proved and effective workaround to get things done ASAP)
    
    We also have to keep in mind that the player has to still be on a Node positions when done using the elevator.
    An elevator could be seen as a makeshifts, quick and dirty pseudo NavGraph Edge.
    This require a bit of extra care when poisitioning the platform and the involved nodes, and creating our animation.
    (What would be aweseome was if we could instantiate and set up an Animation and Animator at runtime.)

---
+++ Assets/Scripts/PlateformElevator.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlateformElevator : MonoBehaviour
+{
+    public NavGraphNode NodeDown;
+    public NavGraphNode NodeUp;
+
+    private bool upstairs;
+    private GameObject player;
+    private Transform playerParent;
+
+    void Awake()
+    {
+        upstairs = false;
+        player = GameObject.Find("Player");
+        playerParent = player.transform.parent;
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+
+
+    public void GoUp()
+    {
+        if( upstairs 
+            || NavGraphManager.instance.ActiveNode != NodeDown)
+            return;
+
+        NodeDown.LeavingNodeEvent.Invoke();
+
+        player.transform.parent = this.gameObject.transform;
+        GetComponent<Animator>().SetBool("isGoingUp", true);
+    }
+
+    public void GoDown()
+    {
+        if (!upstairs
+            || NavGraphManager.instance.ActiveNode == NodeDown)
+            return;
+
+        NodeUp.LeavingNodeEvent.Invoke();
+
+        player.transform.parent = this.gameObject.transform;
+        GetComponent<Animator>().SetBool("isGoingDown", true);
+    }
+
+    void GoneUp()
+    {
+        //gameObject.transform.position += (NodeUp.transform.position - NodeDown.transform.position);
+        player.transform.parent = playerParent;
+        NodeUp.EnteringNodeEvent.Invoke();
+        upstairs = true;
+
+        GetComponent<Animator>().SetBool("isGoingUp", false);
+    }
+
+    void GoneDown()
+    {
+        //gameObject.transform.position -= (NodeUp.transform.position - NodeDown.transform.position);
+        player.transform.parent = playerParent;
+        NodeDown.EnteringNodeEvent.Invoke();
+        upstairs = false;
+        GetComponent<Animator>().SetBool("isGoingDown", false);
+    }
+
+    public void Toggle()
+    {
+        if(upstairs)
+            GoDown();
+        else
+            GoUp();
+    }
+}
