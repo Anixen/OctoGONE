e5f34ca23f9f6ac8ca3a3398eba775207c9a6f32
Anixen <anixen-dev@lapsote.net>
2017-03-11 02:46:50 +0100

    Trigger : Fixed the logic for enabled and locked state.
    Should also make the enabled and locked bool private to pretect the state/

--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -46,6 +46,9 @@ public class Trigger : MonoBehaviour
 
     public void Enable()
     {
+        if(locked)
+            return;
+
         collider.enabled = true;
     }
 
@@ -56,6 +59,7 @@ public class Trigger : MonoBehaviour
 
     public void Lock()
     {
+        Disable();
         locked = true;
     }
 
